8e5d40c92e4f06734caa3b8c1985590be3239de7
yzerlaut <yann.zerlaut@gmail.com>
2016-08-23 19:16:15 +0200

    ==================== \n if commit works you forgot to commit !!! \n ==========================

--- projects/biophys_awareness/contrib_of_single_spike.py
+++ projects/biophys_awareness/contrib_of_single_spike.py
@@ -111,11 +111,11 @@ if __name__=='__main__':
     # network architecture
     parser.add_argument("--Ne",help="excitatory neuron number", type=int, default=4000)
     parser.add_argument("--Ni",help="inhibitory neuron number", type=int, default=1000)
-    parser.add_argument("--f_ext",help="external drive (Hz)",type=float, default=5.)
+    parser.add_argument("--f_ext",help="external drive (Hz)",type=float, default=4.)
     # stimulation (single spike) properties
     parser.add_argument("--stim_start", help="time of the start for the additional spike (ms)", type=float, default=100.)
     parser.add_argument("--stim_delay",help="we multiply the single spike on the trial at this (ms)",type=float, default=50.)
-    parser.add_argument("--stim_jitter",help="we jitter the spike times with a gaussian distrib (ms)",type=float, default=4.)
+    parser.add_argument("--stim_jitter",help="we jitter the spike times with a gaussian distrib (ms)",type=float, default=5.)
     parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
     parser.add_argument("-u", "--update_plot", help="plot the figures", action="store_true")
     parser.add_argument("--filename", '-f', help="filename",type=str, default='data.npz')
