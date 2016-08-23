3d095c2554f17c4bb410b96ae64ebf60f98c3e65
yzerlaut <yann.zerlaut@gmail.com>
2016-08-23 20:39:17 +0200

    ready for massive sim

--- projects/thalamic_control_of_PYR_polarization_sign/various_intensity_levels.py
+++ projects/thalamic_control_of_PYR_polarization_sign/various_intensity_levels.py
@@ -22,14 +22,16 @@ def run_sim(args):
 
     NTWK = [{'name':'exc', 'N':args.Ne, 'type':'AdExp'},
             {'name':'inh', 'N':args.Ni, 'type':'LIF'}]
-    AFFERENCE_ARRAY = [{'Q':2., 'N':400, 'pconn':0.1},
-                       {'Q':2., 'N':100, 'pconn':0.1}]
-    rate_array = ramp_rise_then_constant(t_array, 0., 50., 0, args.f_ext)
+    AFFERENCE_ARRAY = [{'Q':args.Qe_thal, 'N':400, 'pconn':0.1},
+                       {'Q':args.Qe_thal, 'N':100, 'pconn':0.1}]
     
-    EXC_ACTS, INH_ACTS, SPK_TIMES, SPK_IDS = [], [], [], []
+    EXC_ACTS, INH_ACTS, MEAN_VM, STD_VM, EXC_RASTER, INH_RASTER = [], [], [], [], [], []
 
-    for seed in range(1, args.nsim+1):
+    INPUT_RATES = np.linspace(args.stim_min, args.stim_max, args.stim_discret)
+    for f_ext in INPUT_RATES:
 
+        rate_array = ramp_rise_then_constant(t_array, 0., 50., 0, f_ext)
+    
         M = get_connectivity_and_synapses_matrix('CONFIG1', number=len(NTWK))
         POPS, RASTER, POP_ACT = build_populations(NTWK, M, with_raster=True, with_pop_act=True)
 
@@ -40,43 +42,38 @@ def run_sim(args):
                                                              t_array,
                                                              rate_array,\
                                                              pop_for_conductance='A',
-                                                             SEED=seed)
-        SYNAPSES = build_up_recurrent_connections(POPS, M, SEED=seed+1)
+                                                             SEED=args.seed)
+        SYNAPSES = build_up_recurrent_connections(POPS, M, SEED=args.seed+1)
+        VM_MONITOR = brian2.StateMonitor(POPS[0], 'V', record=np.random.randint(POPS[0].N, size=args.recorded_neurons))
 
         net = brian2.Network(brian2.collect())
         # manually add the generated quantities
-        net.add(POPS, SYNAPSES, RASTER, POP_ACT, AFF_SPKS, AFF_SYNAPSES) 
+        net.add(POPS, SYNAPSES, RASTER, POP_ACT, AFF_SPKS, AFF_SYNAPSES, VM_MONITOR) 
         net.run(args.tstop*brian2.ms)
 
-        EXC_ACTS.append(POP_ACT[0].smooth_rate(window='flat',\
-                                       width=args.smoothing*brian2.ms)/brian2.Hz)
-        INH_ACTS.append(POP_ACT[1].smooth_rate(window='flat',\
-                                       width=args.smoothing*brian2.ms)/brian2.Hz)
+        exc_act = POP_ACT[0].smooth_rate(window='flat', width=args.smoothing*brian2.ms)/brian2.Hz
+        inh_act = POP_ACT[1].smooth_rate(window='flat', width=args.smoothing*brian2.ms)/brian2.Hz
+        EXC_ACTS.append(exc_act[int(args.trecord/args.DT):].mean())
+        INH_ACTS.append(inh_act[int(args.trecord/args.DT):].mean())
+        MEAN_VM.append(np.array(VM_MONITOR.V)[:,int(args.trecord/args.DT):].mean())
+        STD_VM.append(np.array(VM_MONITOR.V)[:,int(args.trecord/args.DT):].std())
         
-    np.savez(args.filename, args=args, EXC_ACTS=np.array(EXC_ACTS),
-             INH_ACTS=np.array(INH_ACTS), NTWK=NTWK, t_array=t_array,
-             rate_array=rate_array, AFFERENCE_ARRAY=AFFERENCE_ARRAY,
+    np.savez(args.filename, args=args, NTWK=NTWK, INPUT_RATES=INPUT_RATES,
+             EXC_ACTS=np.array(EXC_ACTS),INH_ACTS=np.array(INH_ACTS),
+             MEAN_VM=np.array(MEAN_VM),STD_VM=np.array(STD_VM),
              plot=get_plotting_instructions())
+    
 
 def get_plotting_instructions():
     return """
 args = data['args'].all()
 fig, AX = plt.subplots(2, 1, figsize=(5,7))
 data = np.load('data.npz')
-AX[0].plot(data['t_array'], data['rate_array'], 'b')
-AX[0].plot(data['t_array'], data['EXC_ACTS'].mean(axis=0), 'g')
-AX[0].plot(data['t_array'], data['INH_ACTS'].mean(axis=0), 'r')
-t_zoom = np.linspace(-10, 30, int(40/args.DT)+1)
-trace, counter = 0.*t_zoom, 0
-for spike_times, exc_act in zip(data['SPK_TIMES'], data['EXC_ACTS']):
-    for t_spk in spike_times:
-        i_spk = int(t_spk/args.DT)
-        counter +=1
-        trace += exc_act[i_spk+int(t_zoom[0]/args.DT):i_spk+int(t_zoom[-1]/args.DT)+1]
-        AX[1].plot(t_zoom, exc_act[i_spk+int(t_zoom[0]/args.DT):i_spk+int(t_zoom[-1]/args.DT)+1], 'k-', lw=.5)
-AX[1].plot(t_zoom, trace/counter, 'k-', lw=2)
+print(data['INPUT_RATES'], data['EXC_ACTS'])
+AX[0].plot(data['INPUT_RATES'], data['EXC_ACTS'], 'b')
+AX[0].plot(data['INPUT_RATES'], data['INH_ACTS'], 'r')
 set_plot(AX[0], xlabel='time (ms)', ylabel='pop. act. (Hz)')
-set_plot(AX[1], xlabel='time lag (ms)', ylabel='pop. act. (Hz)')
+AX[1].plot(data['INPUT_RATES'], data['MEAN_VM'], 'k')
 """
 
 
@@ -91,17 +88,20 @@ if __name__=='__main__':
 
     # simulation parameters
     parser.add_argument("--DT",help="simulation time step (ms)",type=float, default=0.1)
-    parser.add_argument("--tstop",help="simulation duration (ms)",type=float, default=200.)
+    parser.add_argument("--tstop",help="simulation duration (ms)",type=float, default=1000.)
+    parser.add_argument("--trecord",help="simulation duration (ms)",type=float, default=400.)
     parser.add_argument("--nsim",help="number of simulations (different seeds used)", type=int, default=1)
+    parser.add_argument("--recorded_neurons",help="number of recorded neurons for Vm", type=int, default=10)
+    parser.add_argument("--seed",help="seed used", type=int, default=3)
     parser.add_argument("--smoothing",help="smoothing window (flat) of the pop. act.",type=float, default=0.5)
     # network architecture
     parser.add_argument("--Ne",help="excitatory neuron number", type=int, default=4000)
     parser.add_argument("--Ni",help="inhibitory neuron number", type=int, default=1000)
-    parser.add_argument("--f_ext",help="external drive (Hz)",type=float, default=4.)
     # stimulation (single spike) properties
-    parser.add_argument("--stim_start", help="time of the start for the additional spike (ms)", type=float, default=100.)
-    parser.add_argument("--stim_delay",help="we multiply the single spike on the trial at this (ms)",type=float, default=50.)
-    parser.add_argument("--stim_jitter",help="we jitter the spike times with a gaussian distrib (ms)",type=float, default=5.)
+    parser.add_argument("--Qe_thal", help="thalamic excitatory weight (nS)", type=float, default=2.)
+    parser.add_argument("--stim_min", help="min stim level (Hz)", type=float, default=0.)
+    parser.add_argument("--stim_max",help="max stim level (Hz)",type=float, default=10.)
+    parser.add_argument("--stim_discret",help="discretization of stim levels",type=int, default=3)
     parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
     parser.add_argument("-u", "--update_plot", help="plot the figures", action="store_true")
     parser.add_argument("--filename", '-f', help="filename",type=str, default='data.npz')
