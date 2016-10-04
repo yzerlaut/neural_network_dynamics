e46b0a349086d498bacf05d87d37633a94a82cca
yzerlaut <yann.zerlaut@gmail.com>
2016-10-04 15:49:20 +0200

    implementing minimization method to find a given pouint of activity

--- projects/variable_inh_synchrony/sim.py
+++ projects/variable_inh_synchrony/sim.py
@@ -1,21 +1,12 @@
 """
 This script sets up an afferent inhomogenous Poisson process onto the populations
 """
-import brian2, string
 import numpy as np
 
 import sys
 sys.path.append('../../')
-from ntwk_build.syn_and_connec_construct import build_populations,\
-    build_up_recurrent_connections,\
-    initialize_to_rest
-from ntwk_build.syn_and_connec_library import get_connectivity_and_synapses_matrix
-from ntwk_stim.waveform_library import double_gaussian, ramp_rise_then_constant
-from ntwk_stim.connect_afferent_input import construct_feedforward_input
-from common_libraries.data_analysis.array_funcs import find_coincident_duplicates_in_two_arrays
 
-
-def run_sim(args):
+def run_sim(args, return_only_exc=False):
     ### SIMULATION PARAMETERS
 
     brian2.defaultclock.dt = args.DT*brian2.ms
@@ -25,8 +16,7 @@ def run_sim(args):
             {'name':'inh', 'N':args.Ni, 'type':'LIF'}]
     AFFERENCE_ARRAY = [{'Q':args.Qe_thal, 'N':args.Ne, 'pconn':args.pconn},
                        {'Q':args.Qe_thal, 'N':args.Ne, 'pconn':args.pconn}]
-    # rate_array = t_array*0.+args.fext
-    rate_array = np.array([args.fext if tt<50 else 0 for tt in t_array])
+    rate_array = np.array([args.fext_kick if tt<kick_length else args.fext_stat for tt in t_array])
     
     EXC_ACTS, INH_ACTS, SPK_TIMES, SPK_IDS = [], [], [], []
 
@@ -43,11 +33,7 @@ def run_sim(args):
         initialize_to_rest(POPS, NTWK) # (fully quiescent State as initial conditions)
 
         AFF_SPKS, AFF_SYNAPSES = construct_feedforward_input(POPS,
-                                                             AFFERENCE_ARRAY,\
-                                                             t_array,
-                                                             rate_array,\
-                                                             pop_for_conductance='A',
-                                                             SEED=seed)
+                    AFFERENCE_ARRAY, t_array, rate_array, pop_for_conductance='A', SEED=seed)
         SYNAPSES = build_up_recurrent_connections(POPS, M, SEED=seed+1)
 
         net = brian2.Network(brian2.collect())
@@ -59,20 +45,49 @@ def run_sim(args):
                                                width=args.smoothing*brian2.ms)/brian2.Hz)
         INH_ACTS.append(POP_ACT[1].smooth_rate(window='flat',\
                                                width=args.smoothing*brian2.ms)/brian2.Hz)
-        
-    np.savez(args.filename, args=args, EXC_ACTS=np.array(EXC_ACTS),
+
+    if return_only_exc:
+        return EXC_ACTS[0]
+    else:
+        np.savez(args.filename, args=args, EXC_ACTS=np.array(EXC_ACTS),
              INH_ACTS=np.array(INH_ACTS), NTWK=NTWK, t_array=t_array,
              rate_array=rate_array, AFFERENCE_ARRAY=AFFERENCE_ARRAY,
-             plot=get_plotting_instructions())
+             plot=get_plotting_instructions(), plot1=plot1(), plot2=plot2(), plot3=plot3())
 
 
-sys.path.append('../../common_libraries')
-from data_analysis.signanalysis import autocorrel
-import matplotlib.pylab as plt
-from graphs.my_graph import set_plot
+def find_given_act_level_and_run_sim(args, desired_act=20.):
 
-def plot_autocorrel(data, dt, tmax=50):
-    fig, ax = plt.subplots(1, figsize=(4,3))
+    args0 = args.copy()
+    args0.tstop = 200. # limiting to 200ms
+    df = 1 # 1 Hz increment by default
+    imin = int(100/args.DT)
+    exc_act = run_sim(args0, return_only_exc=True)
+    while np.abs(exc_act[imin:].mean()-desired_act)>1:
+        if exc_act[imin:].mean()>desired_act:
+            if not above:
+                df /=2.
+            args0.fext_stat -= df
+            above=True
+            print('raising to', args0.fext_stat)
+        if exc_act[imin:].mean()<desired_act:
+            if above:
+                df /=2.
+            args0.fext_stat -= df
+            above=False
+            print('reducing to', args0.fext_stat)
+        exc_act = run_sim(args0, return_only_exc=True)
+    run_sim(args)
+        
+
+def plot_autocorrel(data, dt, tmax=50, with_fig=None):
+    sys.path.append('../../common_libraries')
+    from data_analysis.signanalysis import autocorrel
+    import matplotlib.pylab as plt
+    from graphs.my_graph import set_plot
+    if with_fig is not None:
+        fig, ax = with_fig
+    else:
+        fig, ax = plt.subplots(1, figsize=(4,3))
     fig.subplots_adjust(left=.2, bottom=.2)
     acf, t_shift = autocorrel(data, tmax, dt)
     ax.plot(t_shift, acf, 'k-')
@@ -88,6 +103,31 @@ from sim import *
 for exc_act, inh_act in zip(data['EXC_ACTS'],data['INH_ACTS']):
     POP_ACT_PLOT(data['t_array'], [exc_act, inh_act], pop_act_zoom=[-1,80])
     plot_autocorrel(inh_act[int(100/args.DT):], args.DT, tmax=50)
+"""
+
+# def plot1():
+#     return """
+# args = data['args'].all()
+# from graphs.ntwk_dyn_plot import RASTER_PLOT, POP_ACT_PLOT
+# RASTER_PLOT([1e3*data['EXC_SPIKES'],1e3*data['inh_spk']], [data['exc_ids'],data['inh_ids']], without_fig=True)
+# """
+
+def plot2():
+    return """
+args = data['args'].all()
+print(args)
+from graphs.ntwk_dyn_plot import POP_ACT_PLOT
+for exc_act, inh_act in zip(data['EXC_ACTS'],data['INH_ACTS']):
+    POP_ACT_PLOT(data['t_array'], [exc_act, inh_act], pop_act_zoom=[-1,80], with_fig=(figure, ax))
+"""
+
+def plot3():
+    return """
+args = data['args'].all()
+print(args)
+from sim import *
+for exc_act, inh_act in zip(data['EXC_ACTS'],data['INH_ACTS']):
+    plot_autocorrel(inh_act[int(100/args.DT):], args.DT, tmax=50, with_fig=(figure, ax))
 """
 
 if __name__=='__main__':
@@ -107,21 +147,40 @@ if __name__=='__main__':
     # network architecture
     parser.add_argument("--Ne",help="excitatory neuron number", type=int, default=4000)
     parser.add_argument("--Ni",help="inhibitory neuron number", type=int, default=1000)
-    parser.add_argument("--Qe", help="weight of excitatory spike (0. means default)", type=float, default=0.)
-    parser.add_argument("--Qe_thal", help="weight of excitatory spike (0. means default)", type=float, default=2.)
+    parser.add_argument("--Qe", help="weight of exc. spike (0. means default)", type=float, default=0.)
+    parser.add_argument("--Qe_thal", help="weight of exc. spike (0. means default)", type=float, default=2.)
     parser.add_argument("--Qi", help="weight of inhibitory spike (0. means default)", type=float, default=0.)
     parser.add_argument("--pconn", help="connection proba", type=float, default=0.05)
-    parser.add_argument("--fext",help="baseline external drive (Hz)",type=float, default=4.)
+    parser.add_argument("--fext_kick",help="external drive KICK (Hz)",type=float, default=4.)
+    parser.add_argument("--fext_stat",help="STATIONARY external drive (Hz)",type=float, default=0.)
+    parser.add_argument("--kick_length",help="duration of external drive KICK (ms)",type=float, default=50.)
     # stimulation (single spike) properties
     parser.add_argument("--rise_time", help="time of the rise of the ramp (ms)", type=float, default=1.)
     parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
     parser.add_argument("-u", "--update_plot", help="plot the figures", action="store_true")
+    parser.add_argument("-q", "--qt_plot", help="plot the figures with the QT interface", action="store_true")
     parser.add_argument("--filename", '-f', help="filename",type=str, default='data.npz')
     args = parser.parse_args()
 
     if args.update_plot:
         data = dict(np.load(args.filename))
         data['plot'] = get_plotting_instructions()
+        data['plot2']=plot2();data['plot3']=plot3()
         np.savez(args.filename, **data)
+    elif args.qt_plot:
+        sys.path.append('/Users/yzerlaut/work/common_libraries/')
+        from graphs.qt_plots import *
+        app = QtWidgets.QApplication(sys.argv)
+        main = Window(DATA_LIST=[args.filename], KEYS=['plot2', 'plot3'])
+        main.show()
+        sys.exit(app.exec_())
     else:
-        run_sim(args)
+        import brian2
+        from ntwk_build.syn_and_connec_construct import build_populations,\
+            build_up_recurrent_connections, initialize_to_rest
+        from ntwk_build.syn_and_connec_library import get_connectivity_and_synapses_matrix
+        from ntwk_stim.waveform_library import double_gaussian, ramp_rise_then_constant
+        from ntwk_stim.connect_afferent_input import construct_feedforward_input
+        from common_libraries.data_analysis.array_funcs import find_coincident_duplicates_in_two_arrays
+        find_given_act_level_and_run_sim(args, desired_act=20.)
+        # run_sim(args)
