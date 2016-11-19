b055921694c399c424e4e03b9e33b92c4991e2c8
yzerlaut <yann.zerlaut@gmail.com>
2016-11-19 19:04:13 +0100

    adding script made by zahara

---
+++ projects/biophys_awareness/script_for_yann.py
@@ -0,0 +1,23 @@
+
+python contrib_of_single_spike.py --Qi 5 --Qe 1.7 --fext_stat 1 --filename 'data_qi5_qe5f3_noise_1.npz'
+
+python contrib_of_single_spike.py --Qi 5 --Qe 1.7 --fext_stat 2 --filename 'data_qi5_qe5f3_noise_2.npz'
+
+python contrib_of_single_spike.py --Qi 3 --Qe 1 --fext_stat 3 --filename 'data_qi3_qe1_noise_3.npz'
+
+python contrib_of_single_spike.py --Qi 5 --Qe 0.7 --fext_stat 3 --filename 'data_qi5_qe5f7_noise_3.npz'
+
+python contrib_of_single_spike.py --Qi 5 --Qe 1.7 --fext_stat 5 --filename 'data_qi5_qe5f3_noise_5.npz'
+
+python contrib_of_single_spike.py --Qi 5 --Qe 0.7 --fext_stat 7 --filename 'data_qi5_qe5f7_noise_7.npz'
+
+python contrib_of_single_spike.py --Qi 7 --Qe 1 --fext_stat 16 --filename 'data_qi7_qe1_noise_16.npz'
+
+python contrib_of_single_spike.py --Qi 30 --Qe 1 --fext_stat 8 --filename 'data_qi30_qe1_noise_8.npz'
+
+python contrib_of_single_spike.py --Qi 30 --Qe 4 --fext_stat 8 --filename 'data_qi30_qe4_noise_8.npz'
+
+python contrib_of_single_spike.py --Qi 30 --Qe 7 --fext_stat 8 --filename 'data_qi30_qe7_noise_8.npz' 
+
+
+
