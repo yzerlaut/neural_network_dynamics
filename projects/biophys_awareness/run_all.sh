d56766683c31fae59654edd8dbedb117f57b4b1a
yann@server26 <yann.zerlaut@iit.it>
2017-02-17 14:09:56 +0100

    update run

--- projects/biophys_awareness/run_all.sh
+++ projects/biophys_awareness/run_all.sh
@@ -1,8 +1,8 @@
-Nsim = 2
-TSTOP = 10000
+Nsim=2
+TSTOP=10000
 # RS-FS configuration
 python contrib_of_single_spike.py -f data/active.npz --tstop $TSTOP --nsim $Nsim
 # Vogels-Abbott configuration
-python contrib_of_single_spike.py -f data/VA.npz --NRNe LIF_Vreset_-60 --NRNi LIF_Vreset_-60 --NTWK Vogels-Abbott --fext_stat 0.
+python contrib_of_single_spike.py -f data/VA.npz --NRNe LIF_Vreset_-60 --NRNi LIF_Vreset_-60 --NTWK Vogels-Abbott --fext_stat 0.  --tstop $TSTOP --nsim $Nsim
 # SR configuration
 python contrib_of_single_spike.py -f data/RS.npz --Qi 0.5 --tstop $TSTOP --nsim $Nsim
