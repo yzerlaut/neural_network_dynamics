edd0338b1fe9335a6b37e63d1c81ad29554681da
yzerlaut <yann.zerlaut@gmail.com>
2017-02-21 16:45:34 +0100

    ==================== \n if commit works you forgot to commit !!! \n ==========================

--- projects/biophys_awareness/run_all.sh
+++ projects/biophys_awareness/run_all.sh
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 Nsim=2
 TSTOP=5000
-=======
-Nsim=3
-TSTOP=500000
->>>>>>> 473f5ea89438be766bd01fc1c8faf9080b7fe422
 # RS-FS configuration
 # python contrib_of_single_spike.py -f data/active.npz --tstop $TSTOP --nsim $Nsim
 # SR configuration
