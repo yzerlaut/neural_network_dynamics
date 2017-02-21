5e2aac7914ca78ddfd0bc7d1ff0fa3b97e3f0ea6
yzerlaut <yann.zerlaut@gmail.com>
2017-02-21 15:02:58 +0100

    ==================== \n if commit works you forgot to commit !!! \n ==========================

--- projects/biophys_awareness/run_all.sh
+++ projects/biophys_awareness/run_all.sh
@@ -1,8 +1,8 @@
-Nsim=10
-TSTOP=500000
+Nsim=2
+TSTOP=5000
 # RS-FS configuration
-python contrib_of_single_spike.py -f data/active.npz --tstop $TSTOP --nsim $Nsim
+# python contrib_of_single_spike.py -f data/active.npz --tstop $TSTOP --nsim $Nsim
+# SR configuration
+# python contrib_of_single_spike.py -f data/RS.npz --Qi 0.5 --tstop $TSTOP --nsim $Nsim
 # Vogels-Abbott configuration
 python contrib_of_single_spike.py -f data/VA.npz --NRNe LIF_Vreset_-60 --NRNi LIF_Vreset_-60 --NTWK Vogels-Abbott --fext_stat 0. --tstop $TSTOP --nsim $Nsim --fext_kick 4. --kick_length 50
-# SR configuration
-python contrib_of_single_spike.py -f data/RS.npz --Qi 0.5 --tstop $TSTOP --nsim $Nsim
