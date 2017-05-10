8ebb675b096a493656e8cf82d5b4f90dc95a1b1e
yzerlaut <yann.zerlaut@gmail.com>
2017-05-10 19:48:05 +0200

    adding a 3 pop model (with dsnh) in the demo

--- demo/3pop_model.py
+++ demo/3pop_model.py
@@ -13,11 +13,16 @@ t_array = ntwk.arange(int(tstop/dt))*dt
 ################################################################
 
 NEURONS = [{'name':'Exc', 'N':4000, 'type':'LIF2'},
-           {'name':'Inh', 'N':1000, 'type':'LIF2_Vthre_-55'}]
+           {'name':'Inh', 'N':1000, 'type':'LIF2_Vthre_-53'},
+           {'name':'DsInh', 'N':200, 'type':'LIF2_Vthre_-53'}]
 
-M = ntwk.init_syn_and_conn_matrix(NEURONS, pconn=0.02)
-M[0,0]['Q'], M[0,1]['Q'] = .1, .1 # almost no recurrent excitation
-M[1,0]['Q'], M[1,1]['Q'] = 10, 10
+M = ntwk.init_syn_and_conn_matrix(NEURONS, pconn=0) # 0 connec by default, just get the shape
+# then manually setting the connectivity
+M[0,0]['Q'], M[0,0]['pconn'], M[0,0]['Erev'], M[0,0]['Tsyn'] = 2., 0.05, 0., 5.
+M[0,1]['Q'], M[0,1]['pconn'], M[0,1]['Erev'], M[0,1]['Tsyn'] = 2., 0.05, 0., 5.
+M[1,0]['Q'], M[1,0]['pconn'], M[1,0]['Erev'], M[1,0]['Tsyn'] = 10., 0.05, -80., 5.
+M[1,1]['Q'], M[1,1]['pconn'], M[1,1]['Erev'], M[1,1]['Tsyn'] = 10., 0.05, -80., 5.
+M[2,1]['Q'], M[2,1]['pconn'], M[2,1]['Erev'], M[2,1]['Tsyn'] = 10., 0.02, -80., 5.
 
 NTWK = ntwk.build_populations(NEURONS, M,
                               with_raster=True, with_Vm=4)
@@ -31,12 +36,11 @@ ntwk.build_up_recurrent_connections(NTWK)
 EAff = {'Q':7., 'N':400, 'pconn':0.1}
 faff = 1.
 # # afferent excitation onto cortical excitation and inhibition
-for i, cond in zip(range(2), ['ExcExc', 'ExcInh']): # both on excitation and inhibition
+for i, cond in enumerate(['ExcExc', 'ExcInh', 'ExcDsInh']): # both on excitation and inhibition
     ntwk.construct_feedforward_input(NTWK, NTWK['POPS'][i], EAff, t_array, faff+0.*t_array,
                                      conductanceID=cond,
                                      SEED=int(37*faff+i)%37)
-
-
+        
 ################################################################
 ## --------------- Initial Condition ------------------------ ##
 ################################################################
@@ -61,5 +65,4 @@ ntwk.show()
 for i in range(4):
     plt.plot(NTWK['VMS'][0][i].t/ntwk.ms, NTWK['VMS'][0][i].V/ntwk.mV)
 ntwk.show()
-
 
