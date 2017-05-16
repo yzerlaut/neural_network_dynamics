89dba2fa224da940eb8c0456835bffaebf03340f
yzerlaut <yann.zerlaut@gmail.com>
2017-05-16 19:21:10 +0200

    up

--- demo/with_correlated_ext_drive.py
+++ demo/with_correlated_ext_drive.py
@@ -12,11 +12,11 @@ t_array = ntwk.arange(int(tstop/dt))*dt
 ## ------------- with recurrent connections ----------------- ##
 ################################################################
 
-NEURONS = [{'name':'Exc', 'N':4000, 'type':'LIF2'},
-           {'name':'Inh', 'N':1000, 'type':'LIF2_Vthre_-55'}]
+NEURONS = [{'name':'Exc', 'N':400, 'type':'LIF2'},
+           {'name':'Inh', 'N':100, 'type':'LIF2_Vthre_-55'}]
 
 M = ntwk.init_syn_and_conn_matrix(NEURONS, pconn=0.02)
-M[0,0]['Q'], M[0,1]['Q'] = .1, .1 # almost no recurrent excitation
+M[0,0]['Q'], M[0,1]['Q'] = 2, 2
 M[1,0]['Q'], M[1,1]['Q'] = 10, 10
 
 NTWK = ntwk.build_populations(NEURONS, M,
@@ -28,19 +28,18 @@ ntwk.build_up_recurrent_connections(NTWK)
 ########### AFFERENT INPUTS ###########
 #######################################
 
-EAff = {'Q':7., 'N':400, 'pconn':0.1}
-faff = 1.
+EAff = {'Q':7., 'N':100, 'pconn':0.1}
+faff = 10.
 # # afferent excitation onto cortical excitation and inhibition
 for i, cond in zip(range(2), ['ExcExc', 'ExcInh']): # both on excitation and inhibition
-    ntwk.construct_feedforward_input(NTWK, NTWK['POPS'][i], EAff, t_array, faff+0.*t_array,
-                                     conductanceID=cond,
-                                     with_presynaptic_spikes=True,
-                                     SEED=int(37*faff+i)%37)
+    ntwk.construct_feedforward_input_correlated(NTWK, NTWK['POPS'][i], EAff, t_array, faff+0.*t_array,
+                                                conductanceID=cond,
+                                                with_presynaptic_spikes=True,
+                                                SEED=int(37*faff+i)%37)
 
-
-################################################################
-## --------------- Initial Condition ------------------------ ##
-################################################################
+##############################################
+## --------- Initial Condition ------------ ##
+##############################################
 ntwk.initialize_to_rest(NTWK)
 
 #####################
@@ -51,15 +50,16 @@ network_sim = ntwk.collect_and_run(NTWK, tstop=tstop, dt=dt)
 # ######################
 # ## ----- Plot ----- ##
 # ######################
-ii=0
-for pop in NTWK['RASTER']:
-    plt.plot(pop.t/ntwk.ms, ii+pop.i, 'o')
-    ii+=np.array(pop.i).max()
-ntwk.set_plot(plt.gca(), ['bottom'], xlabel='time (ms)', yticks=[])
-ntwk.show()
-
 for i in range(4):
     plt.plot(NTWK['VMS'][0][i].t/ntwk.ms, NTWK['VMS'][0][i].V/ntwk.mV)
 ntwk.show()
+
+# ii=0
+# for pop in NTWK['RASTER']:
+#     plt.plot(pop.t/ntwk.ms, ii+pop.i, 'o')
+#     ii+=np.array(pop.i).max()
+# ntwk.set_plot(plt.gca(), ['bottom'], xlabel='time (ms)', yticks=[])
+# ntwk.show()
+
 
 
--- ntwk_stim/connect_afferent_input.py
+++ ntwk_stim/connect_afferent_input.py
@@ -70,36 +70,55 @@ def construct_feedforward_input(NTWK, target_pop,
             NTWK['tRASTER_PRE'] = [times]
 
 
-def set_spikes_from_time_varying_rate_synchronous(time_array, rate_array, N, SEED=1):
+def deal_with_multiple_spikes_within_one_bin(indices, times, DT):
+    # nasty loop
+    n=0
+    for tt in np.unique(times):
+        for ii in np.unique(indices):
+            i1 = np.argwhere(((times==tt) & (indices==ii))).flatten()
+            if len(i1)>1:
+                for j, index in enumerate(i1):
+                    times[index] += DT*(-1)**j*int(j/2+.5)
+                    n+=1
+    print('n=',n, 'spikes were shifted by dt to insure no overlapping presynaptic spikes (Brian2 constraint)')
+    return indices, times
+    
+def set_spikes_from_time_varying_rate_correlated(time_array, rate_array, AFF_TO_POP_MATRIX, SEED=1):
     """
     here, we don't assume that all inputs are decorrelated, we actually
     model a population of N neurons and just produce spikes according
     to the "rate_array" frequency
     """
-    np.random.seed(SEED) # setting the seed !
-    
     ## time_array in ms !!
     # so multplying rate array
+
+    Npop_pre = AFF_TO_POP_MATRIX.shape[0] # 
     
-    indices, times = [], []
+    indices, times = np.empty(0, dtype=np.int), np.empty(0, dtype=np.float)
+    true_indices, true_times = [], []
     DT = (time_array[1]-time_array[0])
     
     # trivial way to generate inhomogeneous poisson events
     for it in range(len(time_array)):
-        rdm_num = np.random.random(N)
-        for ii in np.arange(N)[rdm_num<DT*rate_array[it]*1e-3]:
-            indices.append(ii) # all the indices
-            times.append(time_array[it]) # all the same time !
+        rdm_num = np.random.random(Npop_pre)
+        for ii in np.arange(Npop_pre)[rdm_num<DT*rate_array[it]*1e-3]:
+            true_indices.append(ii)
+            true_times.append(time_array[it])
+            indices = np.concatenate([indices, np.array(AFF_TO_POP_MATRIX[ii,:], dtype=int)]) # all the indices
+            times = np.concatenate([times, np.array([time_array[it] for j in range(len(AFF_TO_POP_MATRIX[ii,:]))])])
 
-    return np.array(indices), np.array(times)*brian2.ms
+    indices, times = deal_with_multiple_spikes_within_one_bin(indices, times, DT)
+    return indices, times*brian2.ms, np.array(true_indices), np.array(true_times)*brian2.ms
 
-def construct_feedforward_input_synchronous(NTWK, target_pop,
-                                            afferent_pop,\
-                                            t, rate_array,\
-                                            conductanceID='AA',\
-                                            with_presynaptic_spikes=False,
-                                            with_background={'f0':None, 'seed':0},
-                                            SEED=1):
+
+def construct_feedforward_input_correlated(NTWK, target_pop,
+                                           afferent_pop,\
+                                           t, rate_array,\
+                                           conductanceID='AA',\
+                                           with_presynaptic_spikes=False,
+                                           with_background={'f0':None, 'seed':0},
+                                           AFF_TO_POP_MATRIX=None,
+                                           SEED=1):
     """
     POPS and AFFERENCE_ARRAY should be 1D arrrays as their is only one 
     source population
@@ -108,46 +127,33 @@ def construct_feedforward_input_synchronous(NTWK, target_pop,
     that will be incremented by the afferent input !!
     """
 
-    # number of synapses per neuron
-    Nsyn = afferent_pop['pconn']*afferent_pop['N']
+    np.random.seed(SEED) # setting the seed !
 
-        
-    indices, times = set_spikes_from_time_varying_rate_synchronous(\
-                            t, rate_array,\
-                            afferent_pop['N'], SEED=(SEED+2)**2%100)
+    if AFF_TO_POP_MATRIX is None:
+        AFF_TO_POP_MATRIX = np.array([\
+                np.random.choice(np.arange(target_pop.N), int(afferent_pop['pconn']*afferent_pop['N']))\
+                for k in range(afferent_pop['N'])])
+    indices, times, true_indices, true_times = set_spikes_from_time_varying_rate_correlated(\
+                                                                  t, rate_array, AFF_TO_POP_MATRIX,\
+                                                                  SEED=(SEED+2)**2%100)
 
-    if with_background['f0'] is not None:
-        """
-        sets the possibility of a trial-varying background activity on top a a fixed stim !
-        (achieves by changing with_background['seed'] and keeping SEED)
-        """
-        indices_bg, times_bg = set_spikes_from_time_varying_rate_synchronous(\
-                                                    t, 0.*t+with_background['f0'],\
-                                                    afferent_pop['N'], SEED=with_background['seed'])
-        indices = np.concatenate([indices, indices_bg])
-        times = np.concatenate([times, times_bg])
-    
-    spikes = brian2.SpikeGeneratorGroup(afferent_pop['N'], indices, times)
-    pre_increment = 'G'+conductanceID+' += w'
-    synapse = brian2.Synapses(spikes, target_pop, on_pre=pre_increment,\
+    spikes = brian2.SpikeGeneratorGroup(target_pop.N, indices, times, sorted=False)
+    synapse = brian2.Synapses(spikes, target_pop, on_pre='G'+conductanceID+' += w',\
                               model='w:siemens')
-
-    N_per_cell = int(afferent_pop['pconn']*afferent_pop['N'])
-    i_rdms = np.concatenate([\
-                             np.random.choice(np.arange(afferent_pop['N']), N_per_cell)\
-                             for jjj in range(target_pop.N)])
-    j_fixed = np.concatenate([np.ones(N_per_cell,dtype=int)*jjj for jjj in range(target_pop.N)])
-    synapse.connect(i=i_rdms, j=j_fixed) 
+    synapse.connect('i==j')
+    synapse.w = afferent_pop['Q']*brian2.nS
     
     NTWK['PRE_SPIKES'].append(spikes)
     NTWK['PRE_SYNAPSES'].append(synapse)
     
     if with_presynaptic_spikes:
         if 'iRASTER_PRE' in NTWK.keys():
-            NTWK['iRASTER_PRE'].append(indices)
-            NTWK['tRASTER_PRE'].append(times)
+            NTWK['iRASTER_PRE'].append(true_indices)
+            NTWK['tRASTER_PRE'].append(true_times)
         else: # we create the key
-            NTWK['iRASTER_PRE'] = [indices]
-            NTWK['tRASTER_PRE'] = [times]
+            NTWK['iRASTER_PRE'] = [true_indices]
+            NTWK['tRASTER_PRE'] = [true_times]
+            
+    return AFF_TO_POP_MATRIX
 
             
