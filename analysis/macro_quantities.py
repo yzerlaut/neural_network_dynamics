7afe39d886aed7e32004d4c375737a254167c5f1
yzerlaut <yann.zerlaut@gmail.com>
2017-06-05 20:09:39 +0200

    up synch def

--- analysis/macro_quantities.py
+++ analysis/macro_quantities.py
@@ -44,13 +44,14 @@ def get_synchrony_of_spiking(data, pop='Exc', Tbin=2, Nmax_pairs=200):
         i, j = couples[rdm_picks[r]]
         tspikes_i = tspikes[np.argwhere(ispikes==i).flatten()]
         tspikes_j = tspikes[np.argwhere(ispikes==j).flatten()]
+        # only pairs with spikes are contributing !
         if len(tspikes_i)>1 and len(tspikes_j)>1:
             spk_train_i, _ = np.histogram(tspikes_i, bins=new_t)
             spk_train_j, _ = np.histogram(tspikes_j, bins=new_t)
             SYNCH.append(np.corrcoef(spk_train_i, spk_train_j)[0,1])
-        elif len(tspikes_i)==0 and len(tspikes_j)==0:
-            # no spikes is considered as synchronous behavior
-            SYNCH.append(1)
+        # elif len(tspikes_i)==0 and len(tspikes_j)==0:
+        #     # no spikes is considered as synchronous behavior
+        #     SYNCH.append(1)
             
     if len(SYNCH)>1:
         return np.array(SYNCH).mean()
