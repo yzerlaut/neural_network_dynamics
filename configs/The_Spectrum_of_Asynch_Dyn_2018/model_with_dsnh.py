afbc906bc70306cae689e85c696efad003e852a8
yzerlaut <yann.zerlaut@gmail.com>
2018-03-14 14:38:46 +0100

    adding config with dsnh

---
+++ configs/The_Spectrum_of_Asynch_Dyn_2018/model_with_dsnh.py
@@ -0,0 +1,46 @@
+Model = {
+    ## ---------------------------------------------------------------------------------
+    ### Initialisation by default parameters
+    ## UNIT SYSTEM is : ms, mV, pF, nS, pA, Hz (arbitrary and unconsistent, so see code)
+    ## ---------------------------------------------------------------------------------
+    # numbers of neurons in population
+    'N_RecExc':4000, 'N_RecInh':1000, 'N_AffExc':100, 'N_DsInh':500,
+    # synaptic weights (nS)
+    'Q_RecExc_RecExc':2., 'Q_RecExc_RecInh':2., 
+    'Q_RecInh_RecExc':10., 'Q_RecInh_RecInh':10., 
+    'Q_AffExc_RecExc':4., 'Q_AffExc_RecInh':4., 
+    'Q_AffExc_DsInh':4.,
+    'Q_DsInh_RecInh':10., 
+    # synaptic time constants (ms)
+    'Tse':5., 'Tsi':5.,
+    # synaptic reversal potentials (mV)
+    'Ee':0., 'Ei': -80.,
+    # connectivity parameters (proba.)
+    'p_RecExc_RecExc':0.05, 'p_RecExc_RecInh':0.05, 
+    'p_RecInh_RecExc':0.05, 'p_RecInh_RecInh':0.05, 
+    'p_DsInh_RecInh':0.05, 
+    'p_AffExc_RecExc':0.1, 'p_AffExc_RecInh':0.1, 
+    'p_AffExc_DsInh':0.075,
+    # afferent stimulation (Hz)
+    'F_AffExc':10.,
+    # simulation parameters (ms)
+    'dt':0.1, 'SEED':3, # low by default, see later
+    ## ---------------------------------------------------------------------------------
+    # === cellular properties (based on AdExp), population by population ===
+    # --> Excitatory population (RecExc, recurrent excitation)
+    'RecExc_Gl':10., 'RecExc_Cm':200.,'RecExc_Trefrac':5.,
+    'RecExc_El':-70., 'RecExc_Vthre':-50., 'RecExc_Vreset':-70., 'RecExc_delta_v':0.,
+    'RecExc_a':0., 'RecExc_b': 0., 'RecExc_tauw':1e9,
+    # --> Inhibitory population (RecInh, recurrent inhibition)
+    'RecInh_Gl':10., 'RecInh_Cm':200.,'RecInh_Trefrac':5.,
+    'RecInh_El':-70., 'RecInh_Vthre':-53., 'RecInh_Vreset':-70., 'RecInh_delta_v':0.,
+    'RecInh_a':0., 'RecInh_b': 0., 'RecInh_tauw':1e9,
+    # --> Disinhibitory population (DsInh, disinhibition)
+    'DsInh_Gl':10., 'DsInh_Cm':200.,'DsInh_Trefrac':5.,
+    'DsInh_El':-70., 'DsInh_Vthre':-50., 'DsInh_Vreset':-70., 'DsInh_delta_v':0.,
+    'DsInh_a':0., 'DsInh_b': 0., 'DsInh_tauw':1e9,
+    ## ---------------------------------------------------------------------------------
+    # === afferent population waveform:
+    'Faff1':4.,'Faff2':20.,'Faff3':8.,
+    'DT':900., 'rise':50.
+}
