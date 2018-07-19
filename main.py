2698b4feb5dfc2a6dab179a1d5462d35c28c328a
Yann Zerlaut <yann.zerlaut@gmail.com>
2018-07-19 14:06:19 +0200

    remove the own hdf5 script, only references to the data_analysis module

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from neural_network_dynamics.ntwk_stim.waveform_library import double_gaussian
 # for this you need the data_analysis folder
 # get it at: bitbucket.org/yzerlaut/data_analysis'
 from neural_network_dynamics.recording.load_and_save import write_as_hdf5
+from data_analysis.IO.hdf5 import load_dict_from_hdf5
 # for plotting
 from neural_network_dynamics.analysis.plot_sim import *
 # for analysis of netwok activity
