0432df594de93d30343b122322df7f704d18aa00
Yann Zerlaut <yann.zerlaut@gmail.com>
2019-01-29 11:39:27 +0100

    update for delta thing

--- cells/pulse_protocols.py
+++ cells/pulse_protocols.py
@@ -79,13 +79,25 @@ if __name__=='__main__':
     from graphs.my_graph import set_plot, show
     from graphs.single_cell_plots import *
     # response_to_current_pulse(*current_pulse_sim(vars(args)))
-    VMS, II, SPIKES = [], [], []
-    for amp in [-50, 50, 200]:
-        args.amp = amp
-        t, Vm, I, spikes = current_pulse_sim(vars(args))
-        VMS.append(Vm)
-        II.append(I)
-        SPIKES.append(spikes)
-    response_to_multiple_current_pulse(t, VMS, II, SPIKES)
-    show()
+    # VMS, II, SPIKES = [], [], []
+    # for amp in [-50, 50, 200]:
+    #     args.amp = amp
+    #     t, Vm, I, spikes = current_pulse_sim(vars(args))
+    #     VMS.append(Vm)
+    #     II.append(I)
+    #     SPIKES.append(spikes)
+    # response_to_multiple_current_pulse(t, VMS, II, SPIKES)
+    # show()
     
+    for delta in [0., 1., 2., 4.]:
+        args.NRN = 'EIF_deltaV_'+str(delta)
+        VMS, II, SPIKES = [], [], []
+        for amp in [-150, 50, 250]:
+            args.amp = amp
+            t, Vm, I, spikes = current_pulse_sim(vars(args))
+            VMS.append(Vm)
+            II.append(I)
+            SPIKES.append(spikes)
+        fig, ax = response_to_multiple_current_pulse(t, VMS, II, SPIKES)
+        fig.suptitle('$\delta$=%imV' % delta)
+        fig.savefig(desktop+'fig+%i.svg' % delta)
