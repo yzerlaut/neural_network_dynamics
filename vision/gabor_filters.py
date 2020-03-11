da3360f0a13de16f80be79c138e246c5f9117b7f
Yann <yann.zerlaut@icm-institute.org>
2020-03-11 20:18:59 +0100

    adding vision model draft

--- vision/gabor_filters.py
+++ vision/gabor_filters.py
@@ -1,40 +1,107 @@
+"""
+
+A gabor filter is paramterized by:
+- $\theta$ the gabor filter orientation , let's write \vec{u} the unitary vector 
+- $f$ the gabor filter frequency
+- $\beta$ the gabor filter excentricity
+- $\Phi$ the gabor filter size
+
+
+And it is defined by:
+
+$$
+G(\vec{x}) = \cos\Big( 2 \pi f (\vec{x}-\vec{x_0}) \cdot \vec{u} + \Phi \Big) \,
+         e^{\frac{\Big((\vec{x}-\vec{x_0}) \cdot \vec{u}\Big)^2 +
+                   \beta \Big((\vec{x}-\vec{x_0}) \cdot \vec{v}\Big)^2}{2 \, \alpha^2}}
+$$
+
+"""
 import numpy as np
 from datavyz.main import graph_env
 
 
 def gabor(x, y,
-          sigma=1, theta=1, Lambda=1, psi=1, gamma=1):
+          x0=0, y0=0,
+          freq = 1.,
+          size=0.5,
+          beta = 1,
+          theta=0.,
+          psi=0):
     """
-    Gabor feature extraction.
+    Gabor filter
     """
-    sigma_x = sigma
-    sigma_y = float(sigma) / gamma
-
-    # Bounding box
-    nstds = 3  # Number of standard deviation sigma
-    xmax = max(abs(nstds * sigma_x * np.cos(theta)), abs(nstds * sigma_y * np.sin(theta)))
-    xmax = np.ceil(max(1, xmax))
-    ymax = max(abs(nstds * sigma_x * np.sin(theta)), abs(nstds * sigma_y * np.cos(theta)))
-    ymax = np.ceil(max(1, ymax))
-    xmin = -xmax
-    ymin = -ymax
-    # (y, x) = np.meshgrid(np.arange(ymin, ymax + 1), np.arange(xmin, xmax + 1))
-
-    # Rotation
-    x_theta = x * np.cos(theta) + y * np.sin(theta)
-    y_theta = -x * np.sin(theta) + y * np.cos(theta)
-
-    gb = np.exp(-.5 * (x_theta ** 2 / sigma_x ** 2 + y_theta ** 2 / sigma_y ** 2)) * np.cos(2 * np.pi / Lambda * x_theta + psi)
+    
+    # Centering and Rotation
+    x_theta = (x-x0) * np.cos(theta) + (y-y0) * np.sin(theta)
+    y_theta = -(x-x0) * np.sin(theta) + (y-y0) * np.cos(theta)
+    
+    # Computing cosinus and gaussian product
+    gb = np.exp(-.5 * (x_theta**2 + beta * y_theta**2) / size**2) *\
+        np.cos(2 * np.pi * freq * x_theta + psi)
+    
     return gb
 
 
-ge = graph_env('visual_stim')
+if __name__=='__main__':
 
-x, y = np.meshgrid(np.linspace(-5, 5), np.linspace(-5, 5))
-z = gabor(x, y)
+    import string, itertools
+    ge = graph_env('manuscript')
 
-fig, _, _ = ge.twoD_plot(x.flatten(), y.flatten(), z.flatten(), colormap=ge.binary)
+    fig, AX = ge.figure(axes=(3,3), figsize=(.75,.9), hspace=.9, wspace=.9,
+                        bottom=.3, left=.4)
 
-fig.savefig('fig.png')
+    x, y = np.meshgrid(np.linspace(0, 4), np.linspace(0, 4))
 
-# ge.show()
+    # standard gabor
+    z = gabor(x, y, x0=2 ,y0=2)
+    ge.twoD_plot(x.flatten(), y.flatten(), z.flatten()+0.5, vmin=0, vmax=1, colormap=ge.binary,
+                 ax=AX[0][0])
+    ge.annotate(AX[0][0], 'see legend', (0.5, 1.05), ha='center')
+    # rotated
+    z = gabor(x, y, x0=2,y0=2,theta=np.pi/4.)
+    ge.twoD_plot(x.flatten(), y.flatten(), z.flatten()+0.5, vmin=0, vmax=1, colormap=ge.binary,
+                 ax=AX[0][1])
+    ge.annotate(AX[0][1], r'$\theta$=$\pi$/4', (0.5, 1.05), ha='center')
+    # shifted location
+    z = gabor(x, y, x0=3 ,y0=3)
+    ge.twoD_plot(x.flatten(), y.flatten(), z.flatten()+0.5, vmin=0, vmax=1, colormap=ge.binary,
+                 ax=AX[0][2])
+    ge.annotate(AX[0][2], r'$\vec{x_0}$=(3,3)', (0.5, 1.05), ha='center')
+    # phase shifted
+    z = gabor(x, y, x0=2 ,y0=2, psi=np.pi)
+    ge.twoD_plot(x.flatten(), y.flatten(), z.flatten()+0.5, vmin=0, vmax=1, colormap=ge.binary,
+                 ax=AX[1][0])
+    ge.annotate(AX[1][0], r'$\Psi$=$\pi$', (0.5, 1.05), ha='center')
+    # frequency
+    z = gabor(x, y, x0=2 ,y0=2, freq=0.5)
+    ge.twoD_plot(x.flatten(), y.flatten(), z.flatten()+0.5, vmin=0, vmax=1, colormap=ge.binary,
+                 ax=AX[1][1])
+    ge.annotate(AX[1][1], r'f=1/2', (0.5, 1.05), ha='center')
+    # frequency
+    z = gabor(x, y, x0=2 ,y0=2, freq=2.)
+    ge.twoD_plot(x.flatten(), y.flatten(), z.flatten()+0.5, vmin=0, vmax=1, colormap=ge.binary,
+                 ax=AX[1][2])
+    ge.annotate(AX[1][2], r'f=.2', (0.5, 1.05), ha='center')
+    # increased excentricity
+    z = gabor(x, y, x0=2 ,y0=2, beta=2)
+    ge.twoD_plot(x.flatten(), y.flatten(), z.flatten()+0.5, vmin=0, vmax=1, colormap=ge.binary,
+                 ax=AX[2][0])
+    ge.annotate(AX[2][0], r'$\beta$=2', (0.5, 1.05), ha='center')
+    # shrkined
+    z = gabor(x, y, x0=2 ,y0=2, size=0.25)
+    ge.twoD_plot(x.flatten(), y.flatten(), z.flatten()+0.5, vmin=0, vmax=1, colormap=ge.binary,
+                 ax=AX[2][1])
+    ge.annotate(AX[2][1], r'$\size$=0.25', (0.5, 1.05), ha='center')
+    # expanded
+    z = gabor(x, y, x0=2 ,y0=2, size=1., freq=0.5)
+    ge.twoD_plot(x.flatten(), y.flatten(), z.flatten()+0.5, vmin=0, vmax=1, colormap=ge.binary,
+                 ax=AX[2][2])
+    ge.annotate(AX[2][2], r'$\sigma$=1,f=1/2', (0.5, 1.05), ha='center')
+    
+    for l, ax in zip(list(string.ascii_lowercase), itertools.chain(*AX)):
+        ge.top_left_letter(ax, l+'  ')
+        ax.set_xticks([0,2,4])
+        ax.set_yticks([0,2])
+        
+    fig.savefig('fig.png')
+
---
+++ vision/layer4_ntwk.py
@@ -0,0 +1,55 @@
+import numpy as np
+from datavyz.main import graph_env
+
+import itertools, string, sys, pathlib
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
+
+from vision.gabor_filters import gabor
+
+N=5 # picking 10 cells
+
+ge = graph_env('visual_stim')
+
+fig, ax = ge.figure()
+
+SCREEN = {'width':16./9.*8, # inches
+          'height':8.,
+          'dpi':20}
+
+Xmax, Ymax = int(SCREEN['width']*SCREEN['dpi']), int(SCREEN['height']*SCREEN['dpi'])
+x, y = np.meshgrid(1.0*np.arange(Xmax), 1.0*np.arange(Ymax))
+
+RANGES = {'x0':[0.1*Xmax, 0.9*Xmax],
+          'y0':[0.1*Ymax, 0.9*Ymax],
+          'freq':[1./(0.05*Ymax), 1./(0.1*Ymax)],
+          'size':[0.02*Ymax, 0.1*Ymax],
+          'beta':[1., 2.],
+          'theta':[0., np.pi/2.],
+          'psi':[0., np.pi]}
+
+CELLS = {}
+for key in RANGES:
+    CELLS[key] = np.random.uniform(RANGES[key][0], RANGES[key][1], size=N)
+
+print(CELLS)    
+Z = 0*x
+
+for i in range(N):
+    
+    z = gabor(x, y,
+              x0=CELLS['x0'][i],
+              y0=CELLS['y0'][i],
+              freq=CELLS['freq'][i],
+              size=CELLS['size'][i],
+              beta=CELLS['beta'][i],
+              theta=CELLS['theta'][i],
+              psi=CELLS['psi'][i])+0.5
+    Z += z/N
+    
+ge.twoD_plot(x.flatten(), y.flatten(), z.flatten(), vmin=0, vmax=1, colormap=ge.binary,
+             ax=ax)
+
+ge.show()
+#fig.savefig('fig.png')
+
+
