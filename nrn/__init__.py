4a99ac44c69ed99865851923feb081179ed587de
ZERLAUT Yann <yann.zerlaut@icm-institute.org>
2023-10-19 14:46:09 +0200

    up

--- nrn/__init__.py
+++ nrn/__init__.py
@@ -2,3 +2,4 @@ from brian2 import *
 from . import morpho_analysis, morphologies
 from .active_mechanisms import *
 from .synaptic_input import *
+from .morphologies.remove_axon_from_swc_file import make_new_swc as remove_axon_from_swc_file
